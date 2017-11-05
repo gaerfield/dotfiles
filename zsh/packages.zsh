6f47e45598d27ec942659b486b2070a48246cffe
gaerfield <gaerfield@kramhal.de>
2017-11-05 02:36:40 +0100

    fzf ergänzt und history-Einstellungen gesetzt

--- zsh/packages.zsh
+++ zsh/packages.zsh
@@ -3,3 +3,11 @@ zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "chrissicool/zsh-256color"
+zplug "junegunn/fzf", \
+    as:command, \
+    use:"bin/fzf-tmux"
+zplug "junegunn/fzf-bin", \
+    as:command, \
+    from:gh-r, \
+    rename-to:"fzf"
+
