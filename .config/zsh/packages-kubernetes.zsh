d4ed9df5e38c549700ad42fea1fbc20044e96cf5
gaerfield <gaerfield@users.noreply.github.com>
2021-10-24 14:00:55 +0200

    switch from tanka to basic jsonnet

--- .config/zsh/packages-kubernetes.zsh
+++ .config/zsh/packages-kubernetes.zsh
@@ -14,7 +14,8 @@ zcommand from"gh-r" bpick"kubens*" id-as"ahmetb/kubens" ; zload ahmetb/kubectx
 zcommand from"gh-r"; zload derailed/k9s
 
 # jsonnet and jsonnet-bundler
-zcommand from"gh-r" mv"tk* -> tk"; zload grafana/tanka
+#zcommand from"gh-r" mv"tk* -> tk"; zload grafana/tanka
+zcommand from"gh-r"; zload google/jsonnet
 zcommand from"gh-r" mv"jb* -> jb"; zload jsonnet-bundler/jsonnet-bundler
 
 # curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
