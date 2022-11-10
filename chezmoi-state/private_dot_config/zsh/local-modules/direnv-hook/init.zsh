082e71a74d5005017b44fdf802c46b14d3428db2
gaerfield <gaerfield@users.noreply.github.com>
2022-11-10 19:18:38 +0100

    several fixes

--- chezmoi-state/private_dot_config/zsh/local-modules/direnv-hook/init.zsh
+++ chezmoi-state/private_dot_config/zsh/local-modules/direnv-hook/init.zsh
@@ -1,5 +1,5 @@
 (( ${+commands[direnv]} )) && () {
-  local HOOK_LOCATION="$ZSH_CONF/local-modules/direnv-hook/functions"
+  local HOOK_LOCATION="$ZDOTDIR/local-modules/direnv-hook/functions"
   local HOOK_FILE="$HOOK_LOCATION/direnv-hook.zsh"
   if [[ ! -e $HOOK_LOCATION ]]; then
     mkdir -p "$HOOK_LOCATION"
