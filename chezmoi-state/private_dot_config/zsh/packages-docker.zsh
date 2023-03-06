0d1a188ad99bc0c37426bcf58bbbb743c75c72df
gaerfield <gaerfield@users.noreply.github.com>
2023-03-06 11:51:26 +0100

    remove outdated configurations

--- chezmoi-state/private_dot_config/zsh/packages-docker.zsh
+++
@@ -1,5 +0,0 @@
-zcompletion https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
-zcompletion https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose
-
-zcommand from"gh-r" bpick"*Linux*x86*64*"
-zload jesseduffield/lazydocker
--- chezmoi-state/private_dot_config/zsh/packages-gcloud.zsh
+++
@@ -1,9 +0,0 @@
-zsnippet $ZSH_CONF/additional-completions/_gcloud
-
-export TF_CLI_CONFIG_FILE="$XDG_CONFIG_HOME/terraform/terraformrc"
-
-zcommand from"gh-r" \
-  atclone"mkdir -p $XDG_CACHE_HOME/terraform/plugin-cache"
-zload warrensbox/terraform-switcher
-alias tfswitch="tfswitch --bin=$XDG_BIN_HOME/terraform"
-
