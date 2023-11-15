e47eca6d931d1ea917151d3cf2fca719ff385560
gaerfield <gaerfield@users.noreply.github.com>
2023-11-15 08:33:03 +0100

    new abbr and fish plugins

--- chezmoi-state/private_dot_config/private_fish/conf.d/abbr.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/abbr.fish
@@ -1,13 +1,13 @@
 abbr --add v nvim
 abbr --add vim nvim
-abbr --add sconf nvim $XDG_CONFIG_HOME/fish
+abbr --add sconf nvim $__fish_config_dir
 abbr --add shit --position anywhere "sudo $history[1]"
 abbr --add !! --position anywhere "$history[1]"
 abbr --add opn xdg-open
 abbr --add fzf-help fzf_configure_bindings --help
 abbr --add df df -h
 abbr --add ip inxi --ip
-abbr --add cat bat -p
+abbr --add cat bat -P
 abbr --add less bat
 abbr --add ip4 curl -sS4 ip.sb
 abbr --add ip6 curl -sS6 ip.sb
