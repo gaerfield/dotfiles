d00f67621b944a8621f64fc70e08695039f0cfcf
gaerfield <gaerfield@users.noreply.github.com>
2024-01-15 15:01:18 +0100

    new adaptions

--- chezmoi-state/private_dot_config/private_fish/conf.d/abbr.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/abbr.fish
@@ -3,11 +3,10 @@ abbr --add vim nvim
 abbr --add sconf nvim $__fish_config_dir
 abbr --add shit --position anywhere "sudo $history[1]"
 abbr --add !! --position anywhere "$history[1]"
+abbr --add !!c "echo $history[1] | wl-copy"
 abbr --add opn xdg-open
 abbr --add fzf-help fzf_configure_bindings --help
 abbr --add df df -h
-abbr --add ip inxi --ip
-abbr --add cat bat -P
 abbr --add less bat
 abbr --add ip4 curl -sS4 ip.sb
 abbr --add ip6 curl -sS6 ip.sb
@@ -15,6 +14,8 @@ abbr --add top btop
 abbr --add htop top
 abbr abbr-persist --set-cursor "echo abbr --add % >> $__fish_config_dir/conf.d/abbr.fish"
 abbr alias "abbr-persist"
+
+abbr --add scan-network --set-cursor "nmap -sV -open -oG scan-result.log -p %22 172.20.0.0/24"
 
 function edit_file
     echo $EDITOR $argv
@@ -24,3 +25,10 @@ abbr -a edit_texts --position command --regex ".+\.txt" --function edit_file
 
 # nice example
 # abbr 4DIRS --set-cursor=! "$(string join \n -- 'for dir in */' 'cd $dir' '!' 'cd ..' 'end')"
+
+abbr --add code codium
+abbr --add c wl-copy
+abbr --add p wl-paste
+abbr --add scpr rsync -avz --info=progress2 --human-readable
+abbr --add netstat ss
+abbr --add tldr tealdeer
--- chezmoi-state/private_dot_config/private_fish/conf.d/podman.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/podman.fish
@@ -1,1 +1,3 @@
 set -x DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+
+abbr --add dco docker compose
