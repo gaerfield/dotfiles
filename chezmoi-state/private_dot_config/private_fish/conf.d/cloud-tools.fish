2aa8671c60bcf5402ccae4bcfe25a5317df64391
gaerfield <gaerfield@users.noreply.github.com>
2024-01-26 11:28:57 +0100

    istioctl management scripts

--- chezmoi-state/private_dot_config/private_fish/conf.d/cloud-tools.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/cloud-tools.fish
@@ -10,7 +10,7 @@ abbr --add kns kubens
 abbr --add gcloud-ctx --set-cursor "gcloud config configurations activate payfree-%"
 
 if status is-interactive
-    # tide customization - show right side kubectl/gcloud info only on specific commands
-    set -gx tide_show_gcloud_on kubectl helm kubens kubectx stern gcloud
-    set -gx tide_show_kubectl_on kubectl helm kubens kubectx stern gcloud
+  # tide customization - show right side kubectl/gcloud info only on specific commands
+  set -gx tide_show_gcloud_on kubectl helm kubens kubectx stern gcloud
+  set -gx tide_show_kubectl_on kubectl helm kubens kubectx stern gcloud
 end
