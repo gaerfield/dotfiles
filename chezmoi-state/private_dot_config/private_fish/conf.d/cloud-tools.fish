683f875dbee706df182eec1d7471a46604d136ab
gaerfield <gaerfield@users.noreply.github.com>
2024-02-23 10:44:25 +0100

    refactor some configs

--- chezmoi-state/private_dot_config/private_fish/conf.d/cloud-tools.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/cloud-tools.fish
@@ -1,16 +1,31 @@
-set -x TFENV_CONFIG_DIR "$XDG_DATA_HOME/tfenv"
-set -x KUBECONFIG $XDG_CONFIG_HOME/kube/config
-set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
-
-abbr --add k kubectl
-abbr --add krestart kubectl rollout restart deployment
-abbr --add kctx kubectx
-abbr --add kns kubens
-
-abbr --add gcloud-ctx --set-cursor "gcloud config configurations activate payfree-%"
-
-if status is-interactive
-  # tide customization - show right side kubectl/gcloud info only on specific commands
-  set -gx tide_show_gcloud_on kubectl helm kubens kubectx stern gcloud
-  set -gx tide_show_kubectl_on kubectl helm kubens kubectx stern gcloud
+if type -q tfenv
+  set -x TFENV_CONFIG_DIR "$XDG_DATA_HOME/tfenv"
 end
+
+if type -q kubectl
+  set -x KUBECONFIG $XDG_CONFIG_HOME/kube/config
+  abbr --add k kubectl
+  abbr --add krestart kubectl rollout restart deployment
+end
+
+if type -q gcloud
+  set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
+  if status is-interactive
+    # tide customization - show right side kubectl/gcloud info only on specific commands
+    set -gx tide_show_gcloud_on kubectl helm kubens kubectx stern gcloud gcx k kcx kns
+  end
+  abbr --add gcx --set-cursor "gcloud config configurations activate payfree-%"
+end 
+
+if type -q kubectx
+  abbr --add kcx kubectx
+  if status is-interactive
+    # tide customization - show right side kubectl/gcloud info only on specific commands
+    set -gx tide_show_kubectl_on kubectl helm kubens kubectx stern gcloud gcx kcx kns
+  end
+end
+
+if type -q kubens
+  abbr --add kns kubens
+end
+
--- chezmoi-state/private_dot_config/private_fish/conf.d/podman.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/podman.fish
@@ -1,3 +1,7 @@
+if not type -q podman
+  exit 0;
+end
+
 set -x DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
 
 abbr --add dco docker compose
