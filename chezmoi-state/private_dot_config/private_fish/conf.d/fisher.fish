d67730613bdf6bd81865c3e2596a47070f6bceae
gaerfield <gaerfield@users.noreply.github.com>
2024-02-23 10:30:39 +0100

    put fisher related scripts in user directory

--- chezmoi-state/private_dot_config/private_fish/conf.d/fisher.fish
+++ chezmoi-state/private_dot_config/private_fish/conf.d/fisher.fish
@@ -1,4 +1,4 @@
-set fisher_path $__fish_config_dir/fisher
+set fisher_path $__fish_user_data_dir/fisher
 
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
