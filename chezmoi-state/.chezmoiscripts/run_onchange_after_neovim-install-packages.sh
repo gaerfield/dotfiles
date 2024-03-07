3fcc4df614cfeb7785cb9a36bb363cc189645503
gaerfield <gaerfield@users.noreply.github.com>
2024-03-07 11:20:34 +0100

    fix dont fail when already on latest vim-plug version

--- chezmoi-state/.chezmoiscripts/run_onchange_after_neovim-install-packages.sh
+++ chezmoi-state/.chezmoiscripts/run_onchange_after_neovim-install-packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.cache/nvim/undo ~/.cache/nvim/swap ~/.cache/nvim/backup ~/.cache/nvim/view
-nvim +silent +PlugUpdate +PlugUpgrade +qa >> /dev/null
+nvim +silent +PlugUpdate +PlugUpgrade +qa || true # ignore errors on PlugUpgrade
