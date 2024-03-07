8985978c7a6bf507903982373592516f79021126
gaerfield <gaerfield@users.noreply.github.com>
2024-03-07 01:13:40 +0100

    dont install binaries already installed

--- chezmoi-state/.chezmoiscripts/run_onchange_before_4_install-aqua.sh
+++ chezmoi-state/.chezmoiscripts/run_onchange_before_4_install-aqua.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# aqua already installed?
+command -v aqua >/dev/null 2>&1 && { exit 0; }
+
 curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.3.0/aqua-installer
 echo "1577b99b74751a5ddeea757198cee3b600fce3ef18990540e4d0e667edcf1b5f  aqua-installer" | sha256sum -c
 chmod +x aqua-installer
